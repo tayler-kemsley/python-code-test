9a8ec30ed916c78fac9becc93dba1514289f7892
Tayler Kemsley <tayler@boughtbymany.com>
2019-07-18 14:49:44 +0100

    Import starships management command

---
+++ shiptrader/management/commands/update_database.py
@@ -0,0 +1,55 @@
+import logging
+
+import requests
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from shiptrader.models import Starship
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def sanitise(self, data):
+        if data.lower() == 'unknown':
+            return None
+        return data
+
+
+    def update_database(self, data):
+        starship = Starship.objects.create(
+            model=data['model'],
+            starship_class=data['starship_class'],
+            manufacturer=data['manufacturer'],
+
+            # Potentially unknown data
+            length=self.sanitise(data['length']).replace(',', ''),
+            hyperdrive_rating=self.sanitise(data['hyperdrive_rating']),
+            passengers=self.sanitise(data['passengers']),
+            crew=self.sanitise(data['crew']),
+            cargo_capacity=self.sanitise(data['cargo_capacity']),
+        )
+        logger.debug(
+            'Starhip added: {} {}'.format(
+                starship.starship_class, starship.manufacturer
+            )
+        )
+
+        self.records_added += 1
+
+    def handle(self, *args, **options):
+        self.records_added = 0
+        url = settings.SWAPI
+        starship_endpoint = url + 'starships/?page=1'
+
+        while True:
+            print('QUERYING: {}'.format(starship_endpoint))
+            results = requests.get(starship_endpoint).json()
+            for result in results['results']:
+                self.update_database(result)
+
+            starship_endpoint = results['next']
+            if not starship_endpoint:
+                break
+
+        self.stdout.write('Records added {}'.format(self.records_added))
--- shiptrader/views.py
+++ shiptrader/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+
+
+class StarShipsAPI(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class StarShipAPI(APIView):
+    def get(self, request, ship_id):
+        pass
+
+    def delete(self, request, ship_id):
+        pass
+
+
+class ListingsAPI(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ListingAPI(APIView):
+    def get(self, request, ship_id):
+        pass
+
+    def delete(self, request, ship_id):
+        pass
